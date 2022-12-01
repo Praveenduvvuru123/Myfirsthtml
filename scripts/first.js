368be464cbd31626b653b059ebbd9d05b0b9a548
praveen <praveenduvvuru1605@gmail.com>
2022-12-01 21:11:01 +0530

    all changes

---
+++ scripts/first.js
@@ -0,0 +1,74 @@
+/*let a = 5;
+let b = 10;
+
+let c=a+b;
+console.logc);*/
+
+//Arithmetic operators
+
+/*let x=6;
+let y=4;
+
+let sum= x+y;
+let Subtraction= x-y;
+let Multiplication= x*y;
+let Division= x/y;
+let Modulus= x%y;
+
+console.log(sum, Subtraction, Multiplication, Division, Modulus);*/
+
+//Comparison operators
+
+/*let x=6;
+let y=4;
+
+console.log(x>y, x<y, x==y, x!=y, x>=y, x<=y);*/
+
+// conditional operators
+/*let age = 30;
+
+if(age>=13 && age<=18){
+    console.log('your are "teenager"');
+}
+else if(age>=19 && age<=29){
+    console.log('your are "adult"');
+} 
+else{
+    console.log("your not a teenager or adult");
+}*/
+
+//Logical operators
+
+/*let a = 5;
+let b = 2;
+
+console.log(a<b && a>b, a<b || a>b, !a<b);*/
+
+//Ternary operators
+
+/*let result = (8<5)?"True":"False";
+console.log(result);*/
+
+//increment and decrement operators
+
+/*let a = 10;
+
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+
+console.log(a);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
