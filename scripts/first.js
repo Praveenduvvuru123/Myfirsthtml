fe2c2f200d5490dfeb5deee1456fbccb7bde9f51
praveen <praveenduvvuru1605@gmail.com>
2022-12-05 11:06:18 +0530

    all changes

--- scripts/first.js
+++ scripts/first.js
@@ -24,7 +24,7 @@ let y=4;
 
 console.log(x>y, x<y, x==y, x!=y, x>=y, x<=y);*/
 
-// conditional operators
+// conditional statment
 /*let age = 30;
 
 if(age>=13 && age<=18){
@@ -36,6 +36,33 @@ else if(age>=19 && age<=29){
 else{
     console.log("your not a teenager or adult");
 }*/
+
+// let num=9;
+// if(num % 2 == 0){
+//     console.log("Divisible by '2'");
+// }
+// else if(num % 3 == 0){
+//     console.log("Divisible by '3'");
+// }
+// else{
+//     console.log("Not Divisible by '2' or '3'");
+// }
+
+// let num = 9;
+// if(num % 2 == 0 && num % 3 == 0){
+//     console.log("Divisible by '2' and '3'");
+// }
+// else{
+//     if(num % 2 == 0){
+//         console.log("Divisible by '2'");
+//     }
+//     else if(num % 3 == 0){
+//         console.log("Divisible by '3'")
+//     }
+//     else{
+//         console.log("Not divisible by '2' or '3'")
+//     }
+// }
 
 //Logical operators
 
@@ -51,24 +78,134 @@ console.log(result);*/
 
 //increment and decrement operators
 
-let a = 10;
+// let a = 10;
 
-console.log(a++);
-console.log(++a);
-console.log(a--);
-console.log(--a);
+// console.log(a++);
+// console.log(++a);
+// console.log(a--);
+// console.log(--a);
 
-console.log(a)
+// console.log(a)
+
+//switch condition
+
+// let number=2;
+//      switch (number){
+    //      case 1:console.log("Monday");
+    //         break;
+//      case 2:console.log("Tuesday");
+//         break;
+//      case 3:console.log("Wednesday");
+//         break;
+//      case 4:console.log("Thursday");
+//         break;
+//      case 5:console.log("Friday");
+//         break;
+//      case 6:console.log("Saturday");
+//         break;
+//      case 7:console.log("Sunday");
+//         break;
+//     default:console.log("invalid number")
+//         break;
+// }
+
+// break statment
+
+// for(let i = 1; i<=10; i++){
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+//continue statment
+
+// for(let i = 1; i<=10; i++){
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i)
+// }
 
 
+//for loop(1 to 10)
+        
+//     for (let i = 1; i<=10; i++) {
+//          console.log(i);
+    
+// }
+
+//for loop(10 to 1)
+
+    // for(let i=10; i>=1; i--){
+        
+    //     console.log(i);
+    // }
+
+    
+// Even number odd number
+
+// let evencount=0;
+// let oddcount=0;
+// for(let i=0;  i<=4; i++);
+// {
+//     let n = 98;
+//     if(n%2==0)
+//     {
+//        evencount++;
+//     }
+//     else
+//     {
+//        oddcount++;
+//     }
+// }
+//     console.log("Even number count is "+evencount);
+//     console.log("odd number count is "+oddcount);
+
+//for Each loop
+
+// let number = [1, 2, 3, 4, 5];
+
+// number.forEach((n)=>{
+//     console.log(n*2);
+// });
+
+//for of loop
+
+// let data=[1,2,3,4,5]
+
+// for(let n of data){
+//     console.log(n,data[4])
+// }
 
 
+//for in loop
+
+// let person = {
+//     name : 'Praveen',
+//     age : '25',
+//     job : 'developer'
+// }
+
+// for (let key in person){
+//     console.log(key);
+// }
 
 
+//while loop
 
+//     let i = 1;
+// while (i<=10) {
+//     console.log(i);
+//     i++;
 
+// }
 
+//Do-while loop
 
-                                                                                                                                                                                                                                                            
-
+// let i = 1;
+// do{
+// console.log(i);
+//      i++;
+// }while(i<=10);
 
