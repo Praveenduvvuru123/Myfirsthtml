d0b1ee5125139a2ac59e803ed9810f1c592c7871
praveen <praveenduvvuru1605@gmail.com>
2022-12-13 11:04:22 +0530

    changes

--- scripts/first.js
+++ scripts/first.js
@@ -514,10 +514,10 @@ console.log(result);*/
 
 // const mySentence = "freeCodeCamp is an awesome resource";
 // const words = mySentence.split(" ");
-
 // for (let i = 0; i < words.length; i++) {
 //     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
 // };
 
 // console.log(words);
+
 
