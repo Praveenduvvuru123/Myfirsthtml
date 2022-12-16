8777873627c469682cbef8780f796f8b91454454
praveen <praveenduvvuru1605@gmail.com>
2022-12-16 12:30:33 +0530

    changes

--- scripts/game.js
+++ scripts/game.js
@@ -3,19 +3,38 @@ let btn2 = document.querySelector('#btnp2');
 let p1value = document.querySelector('#p1value');
 let p2value = document.querySelector('#p2value');
 
+let player1Arr = [];
+let player2Arr = [];
+
 function generateRandom() {
-    let randomNumber = Math.random()*100+1;
+    let randomNumber = (Math.random()*100)+1;
     let result = (Math.floor(randomNumber)); 
     return result;
 }
 
+
 btn1.addEventListener('click', () =>{
-    p1value.innerText = generateRandom();
+    let player1Score = generateRandom();
+    player1Arr.push(player1Score);
+    p1value.innerText = player1Score;
 
 });
 
+function arraySum(numArr) {
+    let sum=0;
+    for(let i =0; i<=5; i++){
+       
+       sum+=numArr[i];
+    }
+    return sum;
+        
+}
+console.log(arraySum(sum));
+
 btn2.addEventListener('click', () =>{
-    p2value.innerText = generateRandom();
+     let player2Score = generateRandom();
+     player2Arr.push(player2Score);
+     p2value.innerText = player2Score;
 });
 
 
