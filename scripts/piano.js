a86f5c46ee3d279bb00b1c283ea8dd7d06ff0652
praveen <praveenduvvuru1605@gmail.com>
2022-12-19 12:01:59 +0530

    changes

---
+++ scripts/piano.js
@@ -0,0 +1,91 @@
+// const pianokeys = document.querySelectorAll(".container1 .key");
+
+let audio = new Audio("./audio/db.mp3");
+
+const playTune1 = () =>{
+    audio.play();
+}
+
+let audio2 = new Audio("./audio/Eb.mp3");
+
+const playTune2 = () =>{
+    audio2.play();
+}
+
+let audio3 = new Audio("./audio/c.mp3");
+
+const playTune3 = () =>{
+    audio3.play();
+}
+
+let audio4 = new Audio("./audio/d.mp3");
+
+const playTune4 = () =>{
+    audio4.play();
+}
+
+let audio5 = new Audio("./audio/E.mp3");
+
+const playTune5 = () =>{
+    audio5.play();
+}
+
+let audio6 = new Audio("./audio/f.mp3");
+
+const playTune6 = () =>{
+    audio6.play();
+}
+
+let audio7 = new Audio("./audio/Gb.mp3");
+
+const playTune7 = () =>{
+    audio7.play();
+}
+
+let audio8 = new Audio("./audio/g.mp3");
+
+const playTune8 = () =>{
+    audio8.play();
+}
+
+let audio9 = new Audio("./audio/A.mp3");
+
+const playTune9 = () =>{
+    audio9.play();
+}
+
+let audio10 = new Audio("./audio/Ab.mp3");
+
+const playTune10 = () =>{
+    audio10.play();
+}
+
+let audio11 = new Audio("./audio/cb.mp3");
+
+const playTune11 = () =>{
+    audio11.play();
+}
+
+let audio12 = new Audio("./audio/bb.mp3");
+
+const playTune12 = () =>{
+    audio12.play();
+}
+
+let audio13 = new Audio("./audio/C5.mp3");
+
+const playTune13 = () =>{
+    audio13.play();
+}
+
+let audio14 = new Audio("./audio/eb.mp3");
+
+const playTune14 = () =>{
+    audio14.play();
+}
+
+
+// pianokeys.forEach(key =>{
+//     key.addEventListener("click",() => playTune(key.dataset.key));
+// });
+
