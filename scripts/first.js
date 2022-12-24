d54e2b9ee8b72fa247dfc3bd582e9a2d706239c0
praveen <praveenduvvuru1605@gmail.com>
2022-12-24 10:30:58 +0530

    changes

--- scripts/first.js
+++ scripts/first.js
@@ -531,3 +531,89 @@ console.log(result);*/
 
 // setInterval(myTimer, 1000);
 
+// const developers = [
+//     {
+//         name      : "john",
+//         favColour : "orange",
+//         company   : "Amazon"
+//     },
+//     {
+//         name      : "rja",
+//         favColour : "blue",
+//         company   : "flipkart"
+//     }
+// ];
+
+// function getDevelopers(){
+//     setTimeout(() => {
+//         developers.forEach((dev)=>{
+//             console.log(dev.company);
+//         });
+//     },2000);
+// }
+
+// getDevelopers();
+
+// function createDeveloper(dev,cb) {
+//     setTimeout(() =>{
+//         developers.push(dev);
+//         cb();
+//     },3000);
+// }
+
+// createDeveloper({
+//     name      : "ramu",
+//     favColour : "white",
+//     company   : "puma"
+// },getDevelopers);
+
+//Promises Function
+
+// function createDeveloper(dev) {
+//     return new Promise((resolve,reject)=> {
+//         setTimeout(()=>{
+//             if(dev && Object.keys(dev).length===0 && Object.getPrototypeOf(dev)===Object.prototype){
+//                 reject("Empty Dev Receieved. Please Fix");
+//             }else{
+//                 resolve(developers.push(dev));
+//             }
+//         },3000);
+//     })
+// }
+
+
+// createDeveloper({
+//     name      : "ramu",
+//     favColour : "white",
+//     company   : "puma"
+// }).then(getDevelopers)
+//   .catch(err=>console.error(err))
+//   .finally(`We're done`);
+
+//Async Await
+
+//     async function sampleProgram(){
+//     await createDeveloper({
+//         name      : "ramu",
+//         favColour : "white",
+//         company   : "puma"
+//     });
+//     getDevelopers();
+// }
+// sampleProgram();
+
+// new Promise( (resolve,reject) => {
+//     setTimeout(() => resolve(1),1000);
+// })
+// .then((result) => {
+//     alert(result);
+//     return result*2;
+// })
+// .then((result) => {
+//     alert(result);
+//     return result*2;
+// })
+// .then((result) => {
+//     alert(result);
+//     return result*2;
+// });
