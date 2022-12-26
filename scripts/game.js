6ed5911912c8017d38e315805c510f8a24e18d3e
praveen <praveenduvvuru1605@gmail.com>
2022-12-26 12:32:01 +0530

    new updates

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,3 @@
-
-
-
 let btn1 = document.querySelector('#btnp1');
 let btn2 = document.querySelector('#btnp2');
 let p1value = document.querySelector('#p1value');
@@ -10,49 +7,56 @@ let player1Arr = [];
 let player2Arr = [];
 let Objfinally = {};
 
+function genarateScors() {
 
-function generateRandom() {
-    let randomNumber = (Math.random()*100)+1;
-    let result = (Math.floor(randomNumber)); 
-    return result;
-}
-
-
-function arraySum(numArr) {
-    let sum=0;
-    for(let i=0; i<5; i++){
-       
-       sum+=numArr[i];
+    function generateRandom() {
+        let randomNumber = (Math.random()*100)+1;
+        let result = (Math.floor(randomNumber)); 
+        return result;
     }
-    return sum;  
-}
 
 
-    btn1.addEventListener('click', () =>{
-    let player1Score = generateRandom();
-    player1Arr.push(player1Score);
-    p1value.innerText = player1Score;
-    console.log(player1Arr);
-    document.getElementById("plr1").innerText = arraySum(player1Arr);
+    function arraySum(numArr) {
+        let sum=0;
+        for(let i=0; i<5; i++){
+            
+            sum+=numArr[i];
+        }
+        return sum;  
+    }
+
     
-});
-    let p1score =  arraySum(player2Arr);
-
-    btn2.addEventListener('click', () =>{
-    let player2Score = generateRandom();
-    player2Arr.push(player2Score);
-    p2value.innerText = player2Score;
-    console.log(player2Arr);
-    document.getElementById("plr2").innerText = arraySum(player2Arr);
-});
-    let p2score =  arraySum(player2Arr);
-
-if(p1score>p2score){
-    console.log("player1 wins")
+        btn1.addEventListener('click', () =>{
+        let player1Score = generateRandom();
+        player1Arr.push(player1Score);
+        p1value.innerText = player1Score;
+        console.log(player1Arr);
+        document.getElementById("plr1").innerText = arraySum(player1Arr);
+        
+    });
+    
+        let p1score =  arraySum(player2Arr);
+    
+        btn2.addEventListener('click', () =>{
+        let player2Score = generateRandom();
+        player2Arr.push(player2Score);
+        p2value.innerText = player2Score;
+        console.log(player2Arr);
+        document.getElementById("plr2").innerText = arraySum(player2Arr);
+    
+    });
+    
+        let p2score =  arraySum(player2Arr);
+    
+    if(p1score>p2score){
+        console.log("player1 wins")
+    }
+    else{
+        console.log("player2 wins")
+    }
 }
-else{
-    console.log("player2 wins")
-}
+
+genarateScors();
 
 
 
